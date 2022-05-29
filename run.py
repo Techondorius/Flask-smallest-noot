93c581d03a88e182f4585b97939629a84e630a22
Techondorius <kurofuku01@gmail.com>
2022-05-29 14:32:45 +0900

    CORS適用

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import sys
 sys.dont_write_bytecode = True
 
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from directory import noot
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(noot.app)
 
 if __name__ == '__main__':
